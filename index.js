fdcfa92b34e8c53ca102ff62bb4d3525ce4b852e
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-04-28 12:29:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ bot.on('guildMemberAdd', member => {
   if (!channel) return;
   channel.send(`Sunucuya HoşGeldin, ${member}!`);
 });
+
+bot.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content.startsWith('n!kick')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.kick('Sebebini yazmalısın!').then(() => {
+          message.reply(`Sunucudan atma başarılı! ${user.tag}`);
+        }).catch(err => {
+          message.reply('Atılacak üyeyi bulamadım.');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to kick!');
+    }
+  }
+});
+
     
 bot.on("message", message => {
 
