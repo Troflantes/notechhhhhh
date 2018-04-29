1783a9b2b8649ff78bdd76f928ef9fee48ef7a80
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-04-29 20:56:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ bot.on('message', message => {
 bot.on('message', msg => {
 if (msg.content.toLowerCase() === prefix + "sigara") {
 msg.channel.send(':smoking: :cloud::cloud::cloud:')
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud:'))
-.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud:'))
 .then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
 }
