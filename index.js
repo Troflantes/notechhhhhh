cf10492ac7163008665afd844061b0a976198b50
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-04-29 21:05:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,10 @@ bot.on('message', message => {
 bot.on('message', msg => {
 if (msg.content.toLowerCase() === prefix + "sigara") {
 msg.channel.send(':smoking: :cloud::cloud::cloud:')
-    
 .then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
-    
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud:'))
-    
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
 .then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
 }
 });
@@ -63,9 +62,9 @@ bot.on('message', message => {
 bot.on('message', msg => {
 if (msg.content.toLowerCase() === prefix + "ping") {
 msg.channel.send('Pingim ölçülüyor..')
-    
-
-    
+.then(nmsg => nmsg.edit("Pingim ölçülüyor.")
+.then(nmsg => nmsg.edit("Pingim ölçülüyor..")
+.then(nmsg => nmsg.edit("Pingim ölçülüyor.")
 .then(nmsg => nmsg.edit("Pingim :ping_pong: " + bot.ping + " Milisaniye"));
 }
 });
