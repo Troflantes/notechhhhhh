f748cbf58c02f981d28e7d114422feace0c46325
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-04-30 19:26:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,38 @@ if (message.content === prefix + "emojiler") {
   const emojiList = message.guild.emojis.map(e=>e.toString()).join(" **|** ");
   message.channel.send(emojiList);
 }
-})
+});
+
+bot.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(' ')[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(' ').slice(1);
+
+  if (command === 'topla') {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p+c);
+    message.channel.sendMessage(`${total}`);
+  }
+  if (command === 'çıkar') {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p-c);
+    message.channel.sendMessage(`${total}`);
+  }
+  if (command === 'çarp') {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p*c);
+    message.channel.sendMessage(`${total}`);
+  }
+  if (command === 'böl') {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p/c);
+    message.channel.sendMessage(`${total}`);
+  }
+});
 
 bot.on("message", message => {
     const dmchannel = bot.channels.find("name", "notechdm");
