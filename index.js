9ffa5da2ceeb5a85bce22c65aaa68c35c7a1b763
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-01 12:09:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,10 +42,54 @@ return msg.channel.send({embed})}
     }
     });
 
+exports.run = (bot, message, args) => {
+  if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  let guild = message.guild
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = guild.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
+  if (reason.length < 1) return message.reply('Ban sebebini yazmalısın.');
+  if (message.mentions.users.size < 1) return message.reply('Kimi banlayacağını yazmalısın.').catch(console.error);
+
+  if (!message.guild.member(user).bannable) return message.reply('Yetkilileri banlayamam.');
+  message.guild.ban(user, 2);
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('Eylem:', 'Sunucudan Yasaklama :bangbang: ')
+    .addField('Yasaklanan Kullanıcı:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Yasaklayan Yetkili:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Yasaklama Sebebi:', reason);
+  return guild.channels.get(modlog.id).sendEmbed(embed);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 2
+}
+
+exports.help = {
+  name: prefix + 'ban',
+  description: 'İstediğiniz kişiyi sunucudan yasaklar.',
+  usage: prefix + 'ban [kullanıcı] [sebep]'
+ }
+});
+
+
 bot.on('message', message => {
   if (!message.guild) return;
 
-  if (message.content.startsWith(prefix + 'ban')) {
+  if (message.content.startsWith(prefix + 'bann')) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
