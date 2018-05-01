a21880be73cf8754fee5294ada23a0c04250839a
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-01 19:26:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ bot.on("ready", () => {
 });
 
 module.exports = bot => {
-var Games = ["${prefix}yardım ✨ 1/3", "${bot.guilds.size} Sunucu 👀 3/3", "${bot.users.size} Kullanıcı 💞 2/3"];
+var Games = [
+    "${prefix}yardım ✨ 1/3",
+    "${bot.guilds.size} Sunucu 👀 3/3",
+    "${bot.users.size} Kullanıcı 💞 2/3"];
     setInterval(function() {
         var random = Math.floor(Math.random()*(Games.length-0+1)+0);
         bot.user.setGame(Games[random], "https://www.twitch.tv/scarew0");
-        }, 2 * 2500);
+        }
 };
 
 bot.login(process.env.BOT_TOKEN);
