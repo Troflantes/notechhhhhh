bb4776a85dcc926678cd6f728f54f077498a975f
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-02 15:03:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,30 @@ bot.on("message", message => {
   }
 });
 
+bot.on('message', (message) => {
+    if (message.author.id !== bot.user.id) return;
+    if (!message.content.startsWith(prefix)) return;
+    let command = message.content.slice(prefix.length);
+    let split = command.split(" ");
+    command = split[0];
+    split.shift();
+    let code = split.join(" ");
+    if (command === "eval") {
+        try {
+        let ev = require('util').inspect(eval(code));
+        if (ev.length > 1950) {
+            ev = ev.substr(0, 1950);
+        }
+        let token = auth.token.replace(/\./g, "\.")
+        let re = new RegExp(token, 'g') 
+        ev = ev.replace(re, "*R-eD-Ac-Te-D-*");
+        message.channel.sendMessage("**Input:**```js\n"+code+"```**Eval:**```js\n"+ev+"```")
+        } catch(err) {
+            message.channel.sendMessage('```js\n'+err+"```")
+        }
+    }
+});
+
 bot.on('message', async msg => {
   if (msg.content.toLowerCase() === prefix + 'yardım') {
     await msg.react('🇹');
