46d8ef81da5528c023f8d982b69dbe584eafb37d
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-02 12:27:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bot = new Discord.Client();
 
 let prefix = "n!";
 let owner = "430011871555223553";
-let owner = "441164897175339010";
 
 bot.on("ready", () => {
     bot.user.setStatus('online');
