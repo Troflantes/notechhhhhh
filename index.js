f4ee1670580b70f412f4b469d5f63d7397da4406
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-02 14:31:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,32 +43,6 @@ bot.on("message", message => {
 });
 
 bot.on("message", message => {
-  const args = message.content.split(" ").slice(1);
-    if (message.content.startsWith(prefix + "eval")) {
-	if (message.author.id === "430011871555223553") {
-		try {
-		  var code = args.join(" ");
-		  var evaled = eval(code);
-
-		  if (typeof evaled !== "string")
-			evaled = require("util").inspect(evaled);
-
-		  message.channel.send("xl", clean(evaled), {code:true});
-		} catch (err) {
-		  message.channel.send(`\`HATA\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-		}
-		function clean(text) {
-		  if (typeof(text) === "string")
-			return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-		  else
-			  return text;
-		}
-	} else {
-		message.reply('Bu komutu kullanmak için gerekli izine sahip değilsin.')
-	}
-};
-
-bot.on("message", message => {
     if (message.content.toLowerCase() === prefix + 'gametoplam') {
     if (message.author.id !== "430011871555223553") {
       message.reply('sie');
