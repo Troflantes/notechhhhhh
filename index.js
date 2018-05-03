7cf2e1f6bb114161ff262db273c139594dabe6bf
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-03 20:38:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -176,6 +176,14 @@ bot.on('message', msg => {
     }
     });
 
+bot.on('message', msg => {
+  if (msg.content.startsWith(prefix + "gönder")) {
+    let mesaj = msg.content.substring(2 + 3);
+    msg.delete();
+    bot.users.get(`msg.author.id`).send("mesaj");
+    }
+    });
+
 bot.on ('message', message => {
 if (message.content.toLowerCase() === prefix + 'emojiler') {
   const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ** | ** ");
