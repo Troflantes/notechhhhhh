f625e64a9798f88672e819175d541f8ff13a2482
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-10 14:43:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -640,12 +640,19 @@ bot.on("message", message => {
       if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled);
 
-      message.channel.send(clean(evaled), {code:"xl"});
+      message.channel.sendCode(clean(evaled), {code:"xl"});
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
   }
 });
+
+const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
 
 bot.login(process.env.BOT_TOKEN);
 
