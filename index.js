725b3d828237046aa722704a8d7ba3c7192e9475
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-25 13:26:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -469,8 +469,10 @@ if (msg.content.toLowerCase() === prefix + "sigara") {
 msg.channel.send(':smoking: :cloud::cloud::cloud:')
 .then(nmsg => nmsg.edit(':smoking: :cloud::cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
+.then(nmsg => nmsg.edit(':smoking: :cloud::cloud:'))
 .then(nmsg => nmsg.edit(':smoking: :cloud:'))
-.then(nmsg => nmsg.edit('[**Sigaram bitti**](https://discord.gg/DZsZsEG) | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
+.then(nmsg => nmsg.edit(':smoking: :cloud:'))
+.then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** :no_smoking: **Sigara Sağlığa Zararlıdır**'));
 }
 });
 
