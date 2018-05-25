72786542ba46be73ab0f66aa1988c360b8cd1361
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-25 12:15:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -507,8 +507,8 @@ bot.on("message", message => {
     }
     
     if (message.content.toLowerCase() === prefix + "ping") {
-	    message.channel.sendMessage(`Bakıyorum..`)
-	    .then(nmsg => nmsg.edit('Hmmm.. ``${Math.round(bot.ping)ms``'));
+	    message.channel.send(`Bakıyorum..`)
+	    .then(nmsg => nmsg.edit(`Hmmm.. (``${Math.round(bot.ping)ms``)`));
     }   
 	
     if (message.content.toLowerCase() === prefix + "botbilgi") {
