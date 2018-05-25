f409d6dd6a417cec93a6376fbda057589130d557
Scarewsss <38795362+Scarewsss@users.noreply.github.com>
2018-05-25 14:47:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,14 +66,14 @@ message.channel.sendEmbed(new Discord.RichEmbed()
 bot.on("message", message => {
     if (message.content.toLowerCase() === prefix + 'afk') {
       message.reply(`Başarılı bir şekilde Away From Keyboard [AFK] oldun.`)
-      message.member.setNickname('[AFK] ' + message.author.username + '');
+      message.author.setNickname('[AFK] ' + message.author.username + '');
   }
 });
 
 bot.on("message", message => {
     if (message.content.toLowerCase() === prefix + 'gldm') {
       message.reply(`Başarılı bir şekilde Away From Keyboard [AFK] durumundan çıktın.`)
-      message.member.setNickname('' + message.author.username + '');
+      message.author.setNickname('' + message.author.username + '');
   }
 });
 
